f583eec669bba5f583ecefe4084be0df35b85f96
neltin <gautofreelance@gmail.com>
2022-12-09 21:24:08 -0300

    prueba de articles

--- components/library/ArticlesList/ArticlesList.js
+++ components/library/ArticlesList/ArticlesList.js
@@ -1,5 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
 export default function ArticlesList(){
+    const [Articles, setArticles] = useState([]);
+    
+    const getArticles = async () => {
+      const res = await fetch('/data.json');
+      const data = await res.json();
+        console.log(data);
+        
+        setArticles(data);
+
+    };
+
+
+    useEffect(() => {
+      getArticles();
+    }, []);
+
+
     return(
-        <h3>ArticlesList</h3>
+        <>
+            <h3>ArticlesList</h3>
+   
+        </>          
     );
 }
