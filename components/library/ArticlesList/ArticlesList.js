c07003b91abe833b4dc171b117f3c42ffebb3929
Ariel <arielfumagalli@gmail.com>
2022-12-21 16:59:32 -0300

    .

--- components/library/ArticlesList/ArticlesList.js
+++ components/library/ArticlesList/ArticlesList.js
@@ -1,70 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import styles from "./ArticlesList.module.scss";
 
+export default function ArticlesList({ data, dataLimit }){
 
-export default function ProductionArticlesList(){
+    const [pagination, setPagination] = React.useState(6);
+    const [dataPagination, setDataPagination] = React.useState(dataLimit);
 
-    // La idea es recibir la data vía props y mostrarla utilizando el método .MAP - Dependiendo de que el index de cada elemento sea par o impar, lo mandaremos en la columna izquierda o derecha.
-
-    const [Articles, setArticles] = useState([]);
-    const [Type, setType] = useState([]);
-    const [Authors, setAuthors] = useState([]);
-    const [Hashtags, setHashtags] = useState([]);
     
 
-    const getArticles = async () => {
-        const res = await fetch('/data.json');
-        const data = await res.json();
-      
-        setArticles(data.articles);
-        setType(data.type);
-        setAuthors(data.authors);
-        setHashtags(data.hashtags);
-    };
-
-
-    useEffect(() => {
-      getArticles();
-    }, []);
-
-
-    return(
-        <>
-
-            {/* La estructura HTML y estilos debería quedar así:
-            
+    return( 
             <div className={styles.wrapper}>
 
-            <div className={styles.cards}>
-
-                <div className={styles.col_left}></div>
-                <div className={styles.col_right}></div>
-
-            </div>
-
-            <button type="button" className={styles.verMas_btn}>Ver más</button> 
-
-            </div> */}
-
-            <h3>ArticlesList</h3>
-            {Articles.map((articles, key) => (
-                <div key={key}>
-                     <strong>{ Type[articles.type] }</strong>
-                        
-                        <h3>{articles.title} Por -   <span> { articles.authors.map(( a ) => Authors[a]) } </span></h3>
-
-                        <p>{ articles.description }</p>
-                        <br></br>
-
-                        <div>
-
-                        { articles.hashtags.map(( h, key) => (
-                            <a key={key}> {Hashtags[h]} &nbsp;</a>
-                        )) }                            
-                        </div>
-                        <br></br>
+                <div className={styles.cards}>
+                   
+                    <div className={styles.col_left}>
+                        {dataPagination.map((data, i) => {
+                            return (
+                                <React.Fragment key={i}>
+                                    { i % 2 === 0 ?
+                                        <div><p>{data.category}</p></div>
+                                        : ''
+                                    }                            
+                                </React.Fragment>                                         
+                            );
+                        })}
                     </div>
-            ))}
-        </>          
+
+                    <div className={styles.col_right}>
+                        {dataPagination.map((data, i) => {
+                            return (
+                                <React.Fragment key={i}>
+                                    { i % 2 !== 0 ?
+                                        <div><p>{data.category}</p></div>
+                                        : ''
+                                    }                            
+                                </React.Fragment>                                         
+                            );
+                        })}
+                    </div>
+
+                </div>
+
+                <button type="button" className={styles.verMas_btn}>Ver más</button> 
+
+            </div>         
     );
 }
--- pages/producciones/index.js
+++ pages/producciones/index.js
@@ -6,7 +6,92 @@ import ExploringBtns from '../../components/library/ExploringBtns/ExploringBtns'
 
 export default function Producciones(){
 
-    // Aquí colocamos el array de objetos (articlesData) con la data que irá al componente ArticlesList
+    const dataArticles = [
+        {
+            category: "Capítulo de libro",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        },
+        {
+            category: "Ponencia",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        }, 
+        {
+            category: "Conferencia",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        }, 
+        {
+            category: "Capítulo de libro",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        },
+        {
+            category: "Ponencia",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        }, 
+        {
+            category: "Conferencia",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        },  
+        {
+            category: "Capítulo de libro",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        },
+        {
+            category: "Ponencia",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        }, 
+        {
+            category: "Conferencia",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        }, 
+        {
+            category: "Capítulo de libro",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        },
+        {
+            category: "Ponencia",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        }, 
+        {
+            category: "Conferencia",
+            title: "Enseñar hoy. Niños, niñas y jóvenes protagonistas: la tecnología como escenario.",
+            description: "Descripción breve, esta palabra, como sabrán muchos de ustedes, permite hacer referencia a algo de extensión o duración corta.",
+            authors: "Christian Milillo",
+            hashtags: [ "#EntornosDigitales" ,"#Docencia" , "#EducaciónEnLínea" ]
+        },  
+    ]
 
     const exploringBtnsData = [
         {title: 'Formación', path: 'formacion'},
@@ -18,7 +103,10 @@ export default function Producciones(){
     <>
         <PageHeading title="<h1><span>Producciones</span></h1>" margin_bottom_type={1} />
         <ProductionsNav />
-        <ArticlesList />
+       
+        <ArticlesList data={dataArticles} dataLimit={dataArticles.slice(0,3)} />
+        
+
         <TextMarquee />
         <ExploringBtns data={exploringBtnsData} />
     </>
