33a069cd5ad831829ce22b39f9344cbe64093901
Ariel <arielfumagalli@gmail.com>
2023-06-02 00:42:51 -0300

    .

--- components/home/HomeHeading/HomeHeading.js
+++ components/home/HomeHeading/HomeHeading.js
@@ -32,7 +32,7 @@ export default function HomeHeading({ title }){
 
     return(
         <header>
-            <h1 className={styles.heading}>Somos un espacio<br className={styles.mobile} /> de <span ref={element} /><br />en educación y tecnologías digitales</h1>
+            <h2 className={styles.heading}>Somos un espacio<br className={styles.mobile} /> de <span ref={element} /><br />en educación y tecnologías digitales</h2>
         </header>
     );
 }
