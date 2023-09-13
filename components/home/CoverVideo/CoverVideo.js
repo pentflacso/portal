86dddd30f3eb9dd4d73f241971321af591901db3
neltin <gautofreelance@gmail.com>
2023-09-13 12:13:27 -0300

    poster video

--- components/home/CoverVideo/CoverVideo.js
+++ components/home/CoverVideo/CoverVideo.js
@@ -7,12 +7,12 @@ export default function CoverVideo({data}){
     return(
         <div className={styles.container}>
             {windowSize >= 1025 ?
-                <video playsInline autoPlay muted loop>
+                <video playsInline autoPlay muted loop poster={data.poster_dk1.url ? data.poster_dk1.url : ""}>
                     {/* <source src="/diploma/e-learning/images/anim-exp-3-elemento-izquierda-video.webm" type="video/webm" /> */}
                     <source src={data.video_dk1} type="video/mp4" />                                          
                 </video>
             :
-                <video playsInline autoPlay muted loop>
+                <video playsInline autoPlay muted loop poster={data.poster_mb1.url ? data.poster_mb1.url: ""}>
                     <source src={data.video_mb1} type="video/mp4" />                                          
                 </video>
             }
