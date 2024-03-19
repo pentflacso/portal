11e60087a3d6432b3ee38125ad309cc6b150e48c
MillandStudio <millandstudio@gmail.com>
2024-03-19 11:40:12 -0300

    redireccionamiento

---
+++ pages/diploma/[index].js
@@ -0,0 +1,18 @@
+// pages/index.js
+const HomePage = () => {
+    return <div>Redireccionando...</div>;
+  };
+  
+  export async function getServerSideProps({ req, res }) {
+    const { url } = req; // Captura la URL actual.
+  
+    if (url) {
+      const destinationURL = `https://flacso.pent.org.ar/diploma`;
+      res.writeHead(302, { Location: destinationURL });
+      res.end();
+    }
+  
+    return { props: {} };
+  }
+  
+  export default HomePage;
