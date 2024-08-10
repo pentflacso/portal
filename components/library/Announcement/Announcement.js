9c387e3b038e6e1057b0d5724d18a16991f6e745
MillandStudio <millandstudio@gmail.com>
2024-08-10 01:34:28 -0300

    ajustes

--- components/library/Announcement/Announcement.js
+++ components/library/Announcement/Announcement.js
@@ -1,27 +1,44 @@
 import { useAppContext } from '../../../context/AppContext';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import styles from "./Announcement.module.scss";
 
 export default function Announcement({data}){
     const { setAnnouncementStatus } = useAppContext();
     const [announcementOutAnimation, setAnnouncementOutAnimation] = useState(false);
+    const [shouldDisplay, setShouldDisplay] = useState(true);
+    const router = useRouter();
+    const currentPath = router.asPath;
+console.log(router.asPath);
+    useEffect(() => {
+        if (data.exception) {
+            const exceptions = data.exception.split(',').map(exc => exc.trim());
+            const matchException = exceptions.some(exception => {
+                const regex = new RegExp(`^${exception.replace('*', '.*')}$`);
+                return regex.test(currentPath);
+            });
+
+            if (matchException) {
+                setShouldDisplay(false);
+            }
+        }
+    }, [data.exception, currentPath]);
 
     function closeAnnouncement() {
         setAnnouncementOutAnimation(true);
         setTimeout(() => {
             setAnnouncementStatus(false);
-        }, "500");    
+        }, 500);    
     }
 
-   if(data != "" ){ 
+    if (shouldDisplay && data.body) {
         return( 
             <div className={!announcementOutAnimation ? `${styles.announcement}` : `${styles.announcement} ${styles.animation_out}` }>   
-
-                <button type="button" aria-label="Cerrar anuncio" className={styles.close_btn} onClick={ () => closeAnnouncement() }><span/><span/></button> 
-                
+                <button type="button" aria-label="Cerrar anuncio" className={styles.close_btn} onClick={closeAnnouncement}><span/><span/></button> 
                 <div id="announcementCTA" className={styles.info} dangerouslySetInnerHTML={{ __html:  data.body }}></div>          
-            
             </div>
         );
+    } else {
+        return null;
     }
 }
--- components/library/SliderCourses/SliderCourses.js
+++ components/library/SliderCourses/SliderCourses.js
@@ -10,6 +10,8 @@ export default function SliderCourses( {dataCourses} ){
     const router = useRouter();
     return (
     <Swiper
+    touchRatio={0.2} // Reducir la sensibilidad
+    threshold={10} 
         modules={[Navigation, FreeMode]}
         spaceBetween={0}
         slidesPerView={"auto"}
@@ -22,15 +24,16 @@ export default function SliderCourses( {dataCourses} ){
              router.asPath !== item.path  ?  (<SwiperSlide key={i}>                           
                     <article className={styles.card}>
                         {item.status == 0 && <InscriptionMarquee data={[{ value: "Inscripción abierta — Inscripción abierta" }]} />}
-                        { item.image && <img src={item.image} alt={item.title} />}
+                        { item.image && <Link href={item.path} ><img src={item.image} alt={item.title} /></Link>}
 
-                        <h5>{item.title}</h5>
+                        <h5><Link href={item.path} >{item.title}</Link></h5>
                         <p>{item.description}</p>
                         <div className={styles.duration_and_modality}>
+                            <p>{item.format}</p>
                             <p>{item.duration}</p>
                             <p>{item.mode}</p>
                         </div>                                    
-                        <Link href={item.path} className="cta_btn">Más información</Link>
+                        <Link href={item.path} className={styles.cta_btn}>Más información</Link>
                     </article>                   
             </SwiperSlide>) : ""
         )}                            
