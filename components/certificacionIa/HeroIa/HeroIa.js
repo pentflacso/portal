771a150e39396cb2218740c81b255259ba7af2bc
Ariel <arielfumagalli@gmail.com>
2024-12-11 15:50:45 -0300

    Landing Certificación IA

---
+++ components/certificacionIa/HeroIa/HeroIa.js
@@ -0,0 +1,38 @@
+import TextMarquee from "../../library/TextMarquee/TextMarquee";
+import styles from "./HeroIa.module.scss";
+
+export default function HeroIa(){
+
+    return(
+        <section className={styles.wrapper}>
+
+            <div className={styles.info}>            
+                <div className={styles.col_left}>
+                    <h1 className={styles.headline}>IA para innovar<br /> en educación</h1>
+                    <p className={styles.description}>Aprendé a diseñar propuestas de enseñanza que integren la inteligencia artificial.</p>
+                    <div className={styles.features}>
+                        <p>Curso de posgrado</p>
+                        <p>14 semanas</p>
+                    </div>
+                </div>
+
+                <div className={styles.col_right}>
+                    <div className={styles.img_wrapper}>
+                        <img src="../assets/images/certificacion_ia/img_hero/element_1.webp" alt="Imagen de portada"/>
+                        <img src="../assets/images/certificacion_ia/img_hero/element_2.webp" alt="Imagen de portada" className={styles.el_2}/>
+                        <img src="../assets/images/certificacion_ia/img_hero/element_3.webp" alt="Imagen de portada" className={styles.el_3}/>
+                        <img src="../assets/images/certificacion_ia/img_hero/element_4.webp" alt="Imagen de portada"
+                        className={styles.el_4}/>
+                        <img src="../assets/images/certificacion_ia/img_hero/element_5.webp" alt="Imagen de portada"
+                        className={styles.el_5}/>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.marquee}>
+                <TextMarquee data={[{ value: "100% VIRTUAL" }]} />
+             </div>   
+           
+        </section>
+    );
+}
---
+++ components/certificacionIa/NavBarIa/NavBarIa.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import Link from "next/link";
+import InterestedModalIa from '../InterestedModalIa/InterestedModalIa';
+import styles from "./NavBarIa.module.scss";
+
+export default function NavBarIa({ refNavBrand, brandVisibility }){
+
+    const [ modal, setModal ] = useState('hidden');  
+
+    return(
+        <>
+            {modal === 'meInteresa' && <InterestedModalIa setModal={setModal} />} 
+
+            <header>        
+                <nav className={`${styles.navbar}`}>                
+
+                    <div className={styles.nav_btns}>
+
+                        <h3 className={brandVisibility ? `${styles.brand}` : `${styles.brand} ${styles.hidden}`} ref={refNavBrand}>
+                            <Link href='/' ><span>FLACSO PENT</span></Link>
+                        </h3>                
+
+                        <div className={brandVisibility ? `${styles.insciption_btn}` : `${styles.insciption_btn} ${styles.active}`}>                            
+                            <p>Inicia el 18 de Junio</p>  
+                               
+                            {/* <a href="https://www.google.com/" rel="noopener noreferrer" className="btn-inscribirme" target="_blank">Inscribirme</a> */}
+
+                            <button type="button" className="btn-inscribirme" onClick={ () => setModal('meInteresa')}>Inscribirme</button> 
+                        </div>                               
+
+                    </div>  
+                </nav> 
+            </header>        
+        </>
+    );
+}
